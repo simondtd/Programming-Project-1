11de245cfc37ff21630898aacbf7ead93dcfc958
Nicolas Nicolas <s3827493@student.rmit.edu.au>
2020-08-13 11:55:52 +1000

    add routes

--- Frontend/game-buddy-finder/src/app/app-routing.module.ts
+++ Frontend/game-buddy-finder/src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { UsersComponent } from './users/users.component';
+import { LoginPageComponent } from './login-page/login-page.component';
 
 const routes: Routes = [
-  {path: 'users', component: UsersComponent}
+  { path: 'users', component: UsersComponent },
+  { path: 'login', component: LoginPageComponent }
+
 ];
 
 @NgModule({
