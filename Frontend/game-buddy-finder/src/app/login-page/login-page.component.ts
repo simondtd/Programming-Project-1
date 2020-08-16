2580aacaefbc2e06554789fb849d541ad7ece612
Nicolas Nicolas <s3827493@student.rmit.edu.au>
2020-08-16 22:46:09 +1000

    ads

--- Frontend/game-buddy-finder/src/app/login-page/login-page.component.ts
+++ Frontend/game-buddy-finder/src/app/login-page/login-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UsersService } from '../services/users.service';
 
 @Component({
   selector: 'app-login-page',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPageComponent implements OnInit {
 
-  constructor() { }
+  constructor(private usersService: UsersService) { }
 
   ngOnInit(): void {
   }
 
+
+  public login(username, password) {
+    this.usersService.
+  }
 }
