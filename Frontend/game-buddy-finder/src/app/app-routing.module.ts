b90cfaffb522f6945b0bef44eb83b7bdfcfc554b
Nicolas Nicolas <s3827493@student.rmit.edu.au>
2020-08-16 22:31:44 +1000

    Remove ignored files

--- Frontend/game-buddy-finder/src/app/app-routing.module.ts
+++ Frontend/game-buddy-finder/src/app/app-routing.module.ts
@@ -2,17 +2,15 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { UsersComponent } from './users/users.component';
 import { LoginPageComponent } from './login-page/login-page.component';
+import { LoginAttemptsComponent } from './login-attempts/login-attempts.component';
+
 
 const routes: Routes = [
   { path: 'users', component: UsersComponent },
-  { path: 'login', component: LoginPageComponent }
+  { path: 'login', component: LoginPageComponent },
+  { path: 'loginattempts', component: LoginAttemptsComponent }
+]
 
-import { LoginAttemptsComponent } from './login-attempts/login-attempts.component';
-
-const routes: Routes = [
-  {path: 'users', component: UsersComponent},
-  {path: 'loginattempts', component: LoginAttemptsComponent}
-];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
