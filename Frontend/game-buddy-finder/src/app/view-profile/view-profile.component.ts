30887a3d66dac5ab431f8f084284f18193f3461a
Nicolas Nicolas <s3827493@student.rmit.edu.au>
2020-08-24 16:19:30 +1000

    view profile

--- Frontend/game-buddy-finder/src/app/view-profile/view-profile.component.ts
+++ Frontend/game-buddy-finder/src/app/view-profile/view-profile.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { UsersService } from '../services/users.service';
+import { ProfilesService } from '../services/profiles.service';
 
 @Component({
   selector: 'app-view-profile',
@@ -9,14 +10,18 @@ import { UsersService } from '../services/users.service';
 export class ViewProfileComponent implements OnInit {
 
   public user;
+  public profile;
 
-  constructor(private usersService: UsersService) {
+  constructor(private usersService: UsersService, private profilesService: ProfilesService) {
     console.log(this.usersService.UserId)
   }
 
   ngOnInit(): void {
     this.usersService.getUser(this.usersService.UserId).subscribe((data) => {
       this.user = data;
+    })
+    this.profilesService.getProfileOfUser(this.usersService.UserId).subscribe((data) => {
+      this.profile = data;
     })
   }
 
