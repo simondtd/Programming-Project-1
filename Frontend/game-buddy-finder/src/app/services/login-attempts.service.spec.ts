c022ca7bc8b0c6808b4bc8bb12d770f64c19a12e
s3694451 <41350107+s3694451@users.noreply.github.com>
2020-08-24 20:36:30 +1000

    Added providers

--- Frontend/game-buddy-finder/src/app/services/login-attempts.service.spec.ts
+++ Frontend/game-buddy-finder/src/app/services/login-attempts.service.spec.ts
@@ -1,13 +1,18 @@
 import { TestBed } from '@angular/core/testing';
-
+import { HttpClientModule } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
+import { HttpHandler } from '@angular/common/http';
 import { LoginAttemptsService } from './login-attempts.service';
 
 describe('LoginAttemptsService', () => {
   let service: LoginAttemptsService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [HttpHandler, HttpClient, HttpClientModule, LoginAttemptsService]
+    });
     service = TestBed.inject(LoginAttemptsService);
+
   });
 
   it('should be created', () => {
