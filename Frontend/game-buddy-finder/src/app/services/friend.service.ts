2fdbe4771a0415def214bf15bfdb831a8c9c5785
s3694451 <41350107+s3694451@users.noreply.github.com>
2020-09-08 14:14:24 +1000

    Added base methods

--- Frontend/game-buddy-finder/src/app/services/friend.service.ts
+++ Frontend/game-buddy-finder/src/app/services/friend.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FriendService {
+  private friendsUrl = environment.baseUrl + 'api/friend';
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
+
+  //Takes in UserID of the user to remove as friend
+  public removeFriend(userId) {
+    return this.httpClient.delete(this.friendsUrl + '/' + userId);
+  }
+
+  public getFriendsOfUser(id) {
+    return this.httpClient.get(this.friendsUrl + '/user/' + id)
+  }
 }
