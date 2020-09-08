536925f7c1b7c57f3ddd28b7359a31984f4886c6
Nicolas Nicolas <s3827493@student.rmit.edu.au>
2020-09-08 14:46:05 +1000

    frontend service

--- Frontend/game-buddy-finder/src/app/friend-request/friend-request.component.ts
+++ Frontend/game-buddy-finder/src/app/friend-request/friend-request.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { UsersService } from '../services/users.service';
+import { FriendRequestService } from '../services/friendrequest.service';
 
 @Component({
   selector: 'app-friend-request',
@@ -6,10 +8,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./friend-request.component.scss']
 })
 export class FriendRequestComponent implements OnInit {
-
-  constructor() { }
+  public friendRequests;
+  constructor(private friendRequestService: FriendRequestService, private usersService: UsersService) { }
 
   ngOnInit(): void {
+    this.friendRequestService.getFriendRequestsOfUser(this.usersService.UserId).subscribe((data) => {
+      this.friendRequests = data;
+
+
+    })
   }
 
 }
