a250110604ae0ce9ff7d66a18faec0b4cdbce6bb
andiysf <64960494+andiysf@users.noreply.github.com>
2020-09-08 21:57:05 +1000

    Unit testing for edit profile

--- Frontend/game-buddy-finder/src/app/edit-profile/edit-profile.component.spec.ts
+++ Frontend/game-buddy-finder/src/app/edit-profile/edit-profile.component.spec.ts
@@ -1,6 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { EditProfileComponent } from './edit-profile.component';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('EditProfileComponent', () => {
   let component: EditProfileComponent;
@@ -8,6 +9,7 @@ describe('EditProfileComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [HttpClientModule, RouterTestingModule.withRoutes([])],
       declarations: [ EditProfileComponent ]
     })
     .compileComponents();
