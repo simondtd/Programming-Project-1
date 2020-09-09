8bee51ba777e3d0c8721b2c45b79bf5963973738
Steven Lee <stevenlin1090@gmail.com>
2020-09-09 15:44:36 +1000

    updated unit tests for friend request

--- Frontend/game-buddy-finder/src/app/services/friendrequest.service.spec.ts
+++ Frontend/game-buddy-finder/src/app/services/friendrequest.service.spec.ts
@@ -1,13 +1,25 @@
-import { TestBed } from '@angular/core/testing';
-
-import { FriendrequestService } from './friendrequest.service';
+import { async, TestBed, ComponentFixture } from '@angular/core/testing';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterTestingModule } from '@angular/router/testing';
+import { FriendRequestService } from './friendrequest.service';
 
 describe('FriendrequestService', () => {
-  let service: FriendrequestService;
+  let service: FriendRequestService;
+  let fixture: ComponentFixture<FriendRequestService>;
 
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientModule, RouterTestingModule.withRoutes([])],
+      declarations: [ FriendRequestService ]
+    })
+    .compileComponents();
+  }));
+  
   beforeEach(() => {
+    fixture = TestBed.createComponent(FriendRequestService);
+    service = fixture.componentInstance;
     TestBed.configureTestingModule({});
-    service = TestBed.inject(FriendrequestService);
+    service = TestBed.inject(FriendRequestService);
   });
 
   it('should be created', () => {
