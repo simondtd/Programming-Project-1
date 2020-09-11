fbd7c681f01b90418e3aebc45ec096ae09863579
andiysf <64960494+andiysf@users.noreply.github.com>
2020-09-11 20:16:17 +1000

    fixed unit testing inbox component

--- Frontend/game-buddy-finder/src/app/inbox/inbox.component.spec.ts
+++ Frontend/game-buddy-finder/src/app/inbox/inbox.component.spec.ts
@@ -2,6 +2,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { InboxComponent } from './inbox.component';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterTestingModule } from '@angular/router/testing';
+import { FormBuilder } from '@angular/forms';
 
 describe('InboxComponent', () => {
   let component: InboxComponent;
@@ -10,16 +11,14 @@ describe('InboxComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [HttpClientModule, RouterTestingModule.withRoutes([])],
-      declarations: [ InboxComponent ]
+      declarations: [InboxComponent],
+      providers: [FormBuilder]
     })
-    .compileComponents();
-
-    fixture = TestBed.createComponent(InboxComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+      .compileComponents();
   }));
 
   beforeEach(() => {
+    TestBed.inject(FormBuilder);
     fixture = TestBed.createComponent(InboxComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
