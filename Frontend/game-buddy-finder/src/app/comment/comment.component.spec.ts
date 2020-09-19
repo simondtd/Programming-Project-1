b601abca408692731fa26415ea6e070a8c6c64eb
Steven Lee <stevenlin1090@gmail.com>
2020-09-19 18:43:33 +1000

    unit testing

--- Frontend/game-buddy-finder/src/app/comment/comment.component.spec.ts
+++ Frontend/game-buddy-finder/src/app/comment/comment.component.spec.ts
@@ -1,6 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { CommentComponent } from './comment.component';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('CommentComponent', () => {
   let component: CommentComponent;
@@ -8,6 +9,7 @@ describe('CommentComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [HttpClientModule, RouterTestingModule.withRoutes([])],
       declarations: [ CommentComponent ]
     })
     .compileComponents();
