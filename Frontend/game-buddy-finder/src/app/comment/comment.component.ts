cc7e754af801f017806d31c503d383b1f18010e4
Steven Lee <stevenlin1090@gmail.com>
2020-09-23 22:52:56 +1000

    feature comment

--- Frontend/game-buddy-finder/src/app/comment/comment.component.ts
+++ Frontend/game-buddy-finder/src/app/comment/comment.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-
+import { PostService } from '../services/post.service';
+import { FormControl } from '@angular/forms';
+import { FormBuilder } from '@angular/forms';
+import { FormGroup } from '@angular/forms';
+import { Comment } from '../models/comment';
+import { UsersService } from '../services/users.service';
 
 @Component({
   selector: 'app-comment',
@@ -7,11 +12,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./comment.component.scss']
 })
 export class CommentComponent implements OnInit {
- 
-  constructor() { }
+  public commentGroup: FormGroup;
+  public comment;
 
-  ngOnInit(): void {
-    
+  constructor(private postService: PostService, private formBuilder: FormBuilder, private UsersService: UsersService) { 
+    this.commentGroup = new FormGroup({
+      postID: new FormControl(),
+      posteruserID: new FormControl(),
+      content: new FormControl()
+    });
   }
 
+  ngOnInit(): void {
+    this.postService.getPosts().subscribe((data) => {
+      this.comment = data;
+    })
+  }
+
+public newComment() {
+  var postID = this.commentGroup.get('postID').value;
+  var posteruserID = this.commentGroup.get('posteruserID').value;
+  var content = this.commentGroup.get('content').value;
+  
+  this.UsersService.getUserByUsername(postID).subscribe((data) => {
+    var postId = data[0].userId
+
+    console.log(postId);
+
+    var senderId = this.UsersService.UserId;
+
+      console.log(senderId);
+      console.log(posteruserID);
+      console.log(content);
+    
+    var comment = new Comment(postId, posteruserID, content);
+    this.postService.addComment(comment);
+  });
+}
 }
