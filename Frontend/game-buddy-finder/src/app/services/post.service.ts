95bb00d6c4650ff5805d6be3dbf384ce172b7bc5
s3694451 <41350107+s3694451@users.noreply.github.com>
2020-09-23 11:43:39 +1000

    Added models and service to frontend

--- Frontend/game-buddy-finder/src/app/services/post.service.ts
+++ Frontend/game-buddy-finder/src/app/services/post.service.ts
@@ -1,4 +1,10 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment';
+import { Router} from '@angular/router';
+import { Subject } from 'rxjs';
+import { User } from '../models/user'
+import { Profile } from '../models/profile'
 
 @Injectable({
   providedIn: 'root'
