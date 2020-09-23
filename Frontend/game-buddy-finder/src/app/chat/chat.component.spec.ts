18dec26701fe41830d380b694b147aa6b48c5e11
rmit-s3640426-adama-fofana <s3640426@student.rmit.edu.au>
2020-09-24 00:26:11 +1000

    fixed error

--- Frontend/game-buddy-finder/src/app/chat/chat.component.spec.ts
+++ Frontend/game-buddy-finder/src/app/chat/chat.component.spec.ts
@@ -1,4 +1,7 @@
+import { HttpClientModule } from '@angular/common/http';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormBuilder } from '@angular/forms';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { ChatComponent } from './chat.component';
 
@@ -8,7 +11,9 @@ describe('ChatComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ChatComponent ]
+      imports: [HttpClientModule, RouterTestingModule.withRoutes([])],
+      declarations: [ ChatComponent ],
+      providers: [FormBuilder]
     })
     .compileComponents();
   }));
