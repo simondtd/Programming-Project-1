280ebda48299d8d679e4824b11a9b6fde6e91852
Steven Lee <stevenlin1090@gmail.com>
2020-09-24 20:43:13 +1000

    unit test fix not 100% done

--- Frontend/game-buddy-finder/src/app/services/clan.service.spec.ts
+++ Frontend/game-buddy-finder/src/app/services/clan.service.spec.ts
@@ -2,6 +2,7 @@ import { async,ComponentFixture,TestBed } from '@angular/core/testing';
 import { HttpClientModule } from '@angular/common/http';
 import { ClanService } from './clan.service';
 import { RouterTestingModule } from '@angular/router/testing';
+import {Clan} from '../models/clan';
 
 describe('ClanService', () => {
   let service: ClanService;
@@ -19,4 +20,17 @@ describe('ClanService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('Clan should be valid', () => {
+    let clan = new Clan(0, "Simon", "clan cool", "Simoncool", "Oceania");
+      expect(service.validateClan(clan)).toBeFalse();
+  });
+
+  it('Clan name should be valid', () => {
+    let clan = new Clan(1, "Steven", "", "", "China");
+      expect(service.validateClan(clan)).toBeTrue();
+  });
+
+
+
 });
--- Frontend/game-buddy-finder/src/app/services/post.service.spec.ts
+++ Frontend/game-buddy-finder/src/app/services/post.service.spec.ts
@@ -1,6 +1,7 @@
 import { HttpClientModule } from '@angular/common/http';
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+import { Post } from '../models/post';
 
 import { PostService } from './post.service';
 
@@ -17,4 +18,22 @@ describe('PostService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('Posts should be valid', () => {
+    let post = new Post(1,"Hey");
+    expect(service.validatePost(post)).toBeTrue();
+  });
+
+  it('Posts should be empty', () => {
+    let post = new Post(null,null);
+    expect(service.validatePost(post)).toBeFalse();
+  });
+
+  it('Comment should be valid', () => {
+    let comment = new Comment(1);
+    expect(service.validateComment(comment.)).toBeTrue();
+  });
+
+
+
 });
