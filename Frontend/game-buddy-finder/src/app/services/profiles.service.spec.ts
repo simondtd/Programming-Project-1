207623efed9222048f8f8423c1a2a3c11afa93a2
rmit-s3640426-adama-fofana <s3640426@student.rmit.edu.au>
2020-09-29 00:52:53 +1000

    update2

--- Frontend/game-buddy-finder/src/app/services/profiles.service.spec.ts
+++ Frontend/game-buddy-finder/src/app/services/profiles.service.spec.ts
@@ -21,20 +21,28 @@ describe('ProfilesService', () => {
   });
 
   it('Profile should be valid', () => {
-    let profile = new Profile(1, "Adama", "Fofana", "AdamaF", "bb", "bb", "adama@gmail.com", "Oceania", "", "1");
+    let profile = new Profile(1, "Adama", "Fofana", "AdamaF", "123", "123", "adama@gmail.com", "Oceania", "", "1");
     expect(service.validateUser(profile)).toBeTrue();
   });
 
   it('Profile should be valid', () => {
-    let profile = new Profile(2, "Steven", "Lee", "StevenL", "cc", "cc", "steven@gmail.com", "Indonesia", "", "1");
+    let profile = new Profile(2, "Steven", "Lee", "StevenL", "123", "123", "steven@gmail.com", "Indonesia", "", "1");
     expect(service.validateUser(profile)).toBeTrue();
   });
   it('Profile should not be valid, passwords not equal', () => {
-    let profile = new Profile(15661, "Steven", "Lee", "StevenL", "cc", "cc", "steven@gmail.com", "Indonesia", "", "1");
+    let profile = new Profile(2, "Steven", "Lee", "StevenL", "45", "12", "steven@gmail.com", "Indonesia", "", "1");
     expect(service.validateUser(profile)).toBeFalse();
   });
   it('Profile should not be valid, username is 0', () => {
-    let profile = new Profile(2, null, "Lee", "StevenL", "cc", "cc", "steven@gmail.com", "Indonesia", "", "1");
+    let profile = new Profile(2, "Steven", "Lee", null, "123", "123", "steven@gmail.com", "Indonesia", "", "1");
+    expect(service.validateUser(profile)).toBeFalse();
+  });
+  it('Profile should not be valid, firstname and lastname is 0', () => {
+    let profile = new Profile(32, null, null, "AdamaF", "123", "123", "adama@gmail.com", "Oceania", "", "1");
+    expect(service.validateUser(profile)).toBeFalse();
+  });
+  it('Profile should not be valid, all is 0', () => {
+    let profile = new Profile(34, null, null, null, null, null, null, null, null, null);
     expect(service.validateUser(profile)).toBeFalse();
   });
 });
