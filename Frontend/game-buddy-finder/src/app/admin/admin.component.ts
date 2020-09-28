d1502682130c8e8b269ca9a2466c83e391c8d43d
andiysf <64960494+andiysf@users.noreply.github.com>
2020-09-28 22:04:05 +1000

    added view clan and user on admin (service not done yet)

--- Frontend/game-buddy-finder/src/app/admin/admin.component.ts
+++ Frontend/game-buddy-finder/src/app/admin/admin.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ClanService } from '../services/clan.service';
+import { UsersService } from '../services/users.service';
+import { Router } from '@angular/router'
+import { FriendService } from '../services/friend.service'
 
 @Component({
   selector: 'app-admin',
@@ -6,10 +10,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./admin.component.scss']
 })
 export class AdminComponent implements OnInit {
-
-  constructor() { }
+  clanView;
+  users;
+  constructor(private clansService: ClanService, private usersService: UsersService, private router: Router, private friendService: FriendService) { }
 
   ngOnInit(): void {
+    this.clansService.getClans().subscribe((data) => {
+      this.clanView = data;
+      console.log(data);
+    })
+    this.usersService.getUsers().subscribe((data) => {
+      this.users = data;
+    })
   }
-
+  public viewClan(clan) {
+    this.clansService.currentClan = clan;
+    this.router.navigate(['/clanpreview'])
+  }
 }
