0e7dfa11f525067b9280ce1c97977e0c1ecc581b
Steven Lee <stevenlin1090@gmail.com>
2020-09-29 15:53:57 +1000

    unit test 90% fixed

--- Frontend/game-buddy-finder/src/app/home-page/home-page.component.spec.ts
+++ Frontend/game-buddy-finder/src/app/home-page/home-page.component.spec.ts
@@ -4,6 +4,8 @@ import { Router } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
 import { HomePageComponent } from './home-page.component';
 import { FormBuilder } from '@angular/forms';
+import { Post } from '../models/post';
+import { PostService } from './post.service';
 
 describe('HomePageComponent', () => {
   let component: HomePageComponent;
@@ -25,5 +27,15 @@ describe('HomePageComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should check length of comments equal to zero', () => {
+    let post = new Post(null,null);
+    expect(HomePageComponent.getCommentsString(post)).toBeTrue();
+  });
+
+  it('should check length of comments equal to one', () => {
+    let post = new Post(1,"Cool Post");
+    expect(HomePageComponent.getCommentsString(post)).toBeTrue();
   });
 });
