6cb41f15cd3b36772dcc29fbae26780f6b0b99bf
rmit-s3640426-adama-fofana <s3640426@student.rmit.edu.au>
2020-09-30 00:37:04 +1000

    Revert "Update home-page.component.spec.ts"
    This reverts commit 2a0421953f6680bc27a88a0927b14426be2457dc.

--- Frontend/game-buddy-finder/src/app/home-page/home-page.component.spec.ts
+++ Frontend/game-buddy-finder/src/app/home-page/home-page.component.spec.ts
@@ -32,7 +32,6 @@ describe('HomePageComponent', () => {
 
   it('should check id and comment is zero', () => {
     let post = new Post(null,null);
-    post.comments = new Array<Comment>(0); //Set comment length to 0
     expect(component.getCommentsString(post)).toBeFalse;
   });
 
