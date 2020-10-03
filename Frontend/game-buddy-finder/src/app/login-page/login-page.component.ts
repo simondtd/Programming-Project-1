5875a4dd298f5b813da71c29ef5dc0d8f80232e7
andiysf <64960494+andiysf@users.noreply.github.com>
2020-10-04 00:20:12 +1000

    Added Folder for md5 and password hash

--- Frontend/game-buddy-finder/src/app/login-page/login-page.component.ts
+++ Frontend/game-buddy-finder/src/app/login-page/login-page.component.ts
@@ -3,6 +3,7 @@ import { UsersService } from '../services/users.service';
 import { FormBuilder } from '@angular/forms';
 import { FormGroup } from '@angular/forms';
 import { FormControl } from '@angular/forms';
+import { md5 } from '../Helper/md5';
 
 
 @Component({
@@ -29,9 +30,9 @@ export class LoginPageComponent implements OnInit {
     //Get username and Password from logingroup
     var username = this.loginGroup.get('username').value;
     var password = this.loginGroup.get('password').value;
-
+    var hash = md5(password);
     //Get the user id
-    this.usersService.login(username, password).subscribe((userId) => {
+    this.usersService.login(username, hash).subscribe((userId) => {
       
       //Get the user with that id
       this.usersService.getUser(userId).subscribe((user) => {
