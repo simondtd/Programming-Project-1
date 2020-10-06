c760de356bc3c5b61e8acd24fc8b9bab79333bd2
andiysf <64960494+andiysf@users.noreply.github.com>
2020-10-06 16:27:08 +1100

    Added button PDF

--- Frontend/game-buddy-finder/src/app/admin/admin.component.ts
+++ Frontend/game-buddy-finder/src/app/admin/admin.component.ts
@@ -67,42 +67,6 @@ export class AdminComponent implements OnInit {
       }
       content += "-------------\n";
     }
-
-    let docDefinition = {
-      header: 'PlayWith Admin -  User Log',
-      content: content
-    };
-
-    pdfMake.createPdf(docDefinition).download("User Log.pdf");
-  }
-
-  public viewClan(clan) {
-    this.clansService.currentClan = clan;
-    this.router.navigate(['/clanpreview'])
-  }
-  public viewUser(users) {
-    this.usersService.searchFriend(users.userName);
-  }
-  public generatePdf() {
-    (window as any).pdfMake.vfs = pdfFonts.pdfMake.vfs;
-    var content = "";
-
-    for (var user of this.users) {
-      content += "USERNAME: " + user.userName + "\n";
-      content += "CREATION TIME: " + user.creationTime + "\n\n";
-
-      content += "SENT MESSAGES: \n\n"
-
-      for (var message of this.messages) {
-        if (message.senderId == user.userId) {
-          content += "TO: " + message.receiverUsername + "\n";
-          content += "DATE: " + message.sendTime + "\n";
-          content += "CONTENT:\n";
-          content += message.content + "\n";
-        }
-      }
-      content += "-------------\n";
-    }
     let docDefinition = {
       header: 'PlayWith Admin -  User Log',
       content: content
