86c07994c2a5efca723c16837eb51ee226c9c15d
andiysf <64960494+andiysf@users.noreply.github.com>
2020-10-06 16:12:46 +1100

    Added button PDF and created

--- Frontend/game-buddy-finder/src/app/admin/admin.component.ts
+++ Frontend/game-buddy-finder/src/app/admin/admin.component.ts
@@ -3,6 +3,10 @@ import { ClanService } from '../services/clan.service';
 import { UsersService } from '../services/users.service';
 import { Router } from '@angular/router'
 import { FriendService } from '../services/friend.service'
+import { MessagesService } from '../services/message.service';
+import { Message } from '../models/message'
+import pdfMake from "pdfmake/build/pdfmake";
+import pdfFonts from "pdfmake/build/vfs_fonts";
 
 @Component({
   selector: 'app-admin',
@@ -12,7 +16,8 @@ import { FriendService } from '../services/friend.service'
 export class AdminComponent implements OnInit {
   clanView;
   users;
-  constructor(private clansService: ClanService, private usersService: UsersService, private router: Router, private friendService: FriendService) { }
+  public messages = Array<Message>();
+  constructor(private clansService: ClanService, private messageService: MessagesService,public usersService: UsersService, private router: Router, private friendService: FriendService) { }
 
   ngOnInit(): void {
     this.clansService.getClans().subscribe((data) => {
@@ -20,6 +25,9 @@ export class AdminComponent implements OnInit {
     })
     this.usersService.getUsers().subscribe((data) => {
       this.users = data;
+    })
+    this.messageService.getMessages().subscribe(data => {
+      this.messages = data;
     })
   }
   public viewClan(clan) {
@@ -29,4 +37,32 @@ export class AdminComponent implements OnInit {
   public viewUser(users) {
     this.usersService.searchFriend(users.userName);
   }
+  public generatePdf() {
+    (window as any).pdfMake.vfs = pdfFonts.pdfMake.vfs;
+    var content = "";
+
+    for (var user of this.users) {
+      content += "USERNAME: " + user.userName + "\n";
+      content += "CREATION TIME: " + user.creationTime + "\n\n";
+
+      content += "SENT MESSAGES: \n\n"
+
+      for (var message of this.messages) {
+        if (message.senderId == user.userId) {
+          content += "TO: " + message.receiverUsername + "\n";
+          content += "DATE: " + message.sendTime + "\n";
+          content += "CONTENT:\n";
+          content += message.content + "\n";
+        }
+      }
+      content += "-------------\n";
+    }
+    let docDefinition = {
+      header: 'PlayWith Admin -  User Log',
+      content: content
+    };
+
+    pdfMake.createPdf(docDefinition).download("User Log.pdf");
+  }
 }
+
