2bd76dc7b6db4a842f1bec0a7d0913ca1b77f339
s3694451 <41350107+s3694451@users.noreply.github.com>
2020-10-06 16:02:13 +1100

    Fixed PDF stuff

--- Frontend/game-buddy-finder/src/app/admin/admin.component.ts
+++ Frontend/game-buddy-finder/src/app/admin/admin.component.ts
@@ -3,6 +3,15 @@ import { ClanService } from '../services/clan.service';
 import { UsersService } from '../services/users.service';
 import { Router } from '@angular/router'
 import { FriendService } from '../services/friend.service'
+import { ProfilesService } from '../services/profiles.service';
+import { InterestService } from '../services/interest.service';
+import { MessagesService } from '../services/message.service';
+import { User } from '../models/user'
+import { Message } from '../models/message'
+import { Profile } from '../models/profile'
+import { Subject } from 'rxjs';
+import pdfMake from "pdfmake/build/pdfmake";
+import pdfFonts from "pdfmake/build/vfs_fonts";
 
 @Component({
   selector: 'app-admin',
@@ -11,10 +20,23 @@ import { FriendService } from '../services/friend.service'
 })
 export class AdminComponent implements OnInit {
   clanView;
-  users;
-  constructor(private clansService: ClanService, private usersService: UsersService, private router: Router, private friendService: FriendService) { }
+
+  public users = Array<User>();
+  public messages = Array<Message>();
+
+  constructor(private messageService: MessagesService,private clansService: ClanService, public usersService: UsersService, private router: Router, private friendService: FriendService) { }
 
   ngOnInit(): void {
+    if (this.usersService.CurrentUser.userType == 1) {
+      this.usersService.getUsers().subscribe(data => {
+        this.users = data;
+      })
+
+      this.messageService.getMessages().subscribe(data => {
+        this.messages = data;
+      })
+    }
+
     this.clansService.getClans().subscribe((data) => {
       this.clanView = data;
     })
@@ -22,6 +44,35 @@ export class AdminComponent implements OnInit {
       this.users = data;
     })
   }
+  public generatePdf() {
+    (window as any).pdfMake.vfs = pdfFonts.pdfMake.vfs;
+    var content = "";
+
+    for (var user of this.users) {
+      content += "USERNAME: " + user.userName + "\n";
+      content += "CREATION TIME: " + user.creationTime + "\n\n";
+
+      content += "SENT MESSAGES: \n\n"
+
+      for (var message of this.messages) {
+        if (message.senderId == user.userId) {
+          content += "TO: " + message.receiverUsername + "\n";
+          content += "DATE: " + message.sendTime + "\n";
+          content += "CONTENT:\n";
+          content += message.content + "\n";
+        }
+      }
+      content += "-------------\n";
+    }
+
+    let docDefinition = {
+      header: 'PlayWith Admin -  User Log',
+      content: content
+    };
+
+    pdfMake.createPdf(docDefinition).download("User Log.pdf");
+  }
+
   public viewClan(clan) {
     this.clansService.currentClan = clan;
     this.router.navigate(['/clanpreview'])
