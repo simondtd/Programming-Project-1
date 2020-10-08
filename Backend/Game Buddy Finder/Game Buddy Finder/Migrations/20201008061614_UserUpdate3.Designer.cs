137195c9239e16f19e861bf75280ae8a3229988b
s3694451 <41350107+s3694451@users.noreply.github.com>
2020-10-08 17:44:12 +1100

    More various fixes

--- Backend/Game Buddy Finder/Game Buddy Finder/Migrations/20201008061614_UserUpdate3.Designer.cs
+++ Backend/Game Buddy Finder/Game Buddy Finder/Migrations/20201008061614_UserUpdate3.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using Game_Buddy_Finder.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Game_Buddy_Finder.Migrations
 {
     [DbContext(typeof(GbfContext))]
-    partial class GbfContextModelSnapshot : ModelSnapshot
+    [Migration("20201008061614_UserUpdate3")]
+    partial class UserUpdate3
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Backend/Game Buddy Finder/Game Buddy Finder/Migrations/20201008061614_UserUpdate3.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Game_Buddy_Finder.Migrations
+{
+    public partial class UserUpdate3 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "SecretAnswer",
+                table: "Users",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "SecretQuestion",
+                table: "Users",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "SecretAnswer",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "SecretQuestion",
+                table: "Users");
+        }
+    }
+}
