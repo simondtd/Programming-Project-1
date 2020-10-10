fb044a77bf84fc7acdb5115809991ce1b162d2e0
s3694451 <41350107+s3694451@users.noreply.github.com>
2020-10-10 11:09:55 +1100

    Added reply functionality to messages

--- Frontend/game-buddy-finder/src/app/services/friend.service.ts
+++ Frontend/game-buddy-finder/src/app/services/friend.service.ts
@@ -1,12 +1,15 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../../environments/environment';
+import { User } from '../models/user';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FriendService {
   private friendsUrl = environment.baseUrl + 'api/friend';
+
+  public FriendsOfUser: Array<User>;
 
   constructor(private httpClient: HttpClient) { }
 
@@ -20,6 +23,11 @@ export class FriendService {
   }
 
   public getFriendsOfUser(userId) {
-    return this.httpClient.get(this.friendsUrl + '/user/' + userId)
+    var obs = this.httpClient.get<User[]>(this.friendsUrl + '/user/' + userId)
+    obs.subscribe(data => {
+      this.FriendsOfUser = data;
+    })
+
+    return obs;
   }
 }
