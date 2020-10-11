bb624a63d8a9c48b03f1934ae72f6b3fe9861a78
s3694451 <41350107+s3694451@users.noreply.github.com>
2020-10-11 12:52:45 +1100

    Started work on backend unit tests

--- Backend/Game Buddy Finder/Game Buddy Finder/Tests/UserTests.cs
+++ Backend/Game Buddy Finder/Game Buddy Finder/Tests/UserTests.cs
@@ -2,10 +2,27 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Moq;
+using Xunit;
+using Game_Buddy_Finder.Models;
+using Game_Buddy_Finder.Controllers;
+using Game_Buddy_Finder.Models.Repository;
 
 namespace Game_Buddy_Finder.Tests
 {
     public class UserTests
     {
+        [Fact]
+        public void User_Test_01()
+        {
+            var mockRepo = new Mock<IDataRepository<User, int>>();
+            mockRepo.Setup(repo => repo.GetAll())
+                .Returns(new List<User>());
+
+            //var controller = new UserController(mockRepo.Object);
+            Assert.Equal(true, true);
+        }
+
+
     }
 }
