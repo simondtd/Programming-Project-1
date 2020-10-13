b8a9bf93e0f314cd888d5ee272a90c071beabd87
s3694451 <41350107+s3694451@users.noreply.github.com>
2020-10-13 14:54:27 +1100

    Added comments to the chat

--- Frontend/game-buddy-finder/src/app/chat/chat.component.ts
+++ Frontend/game-buddy-finder/src/app/chat/chat.component.ts
@@ -1,10 +1,7 @@
 import { Component, OnDestroy, OnInit, AfterViewChecked } from '@angular/core';
 import * as SignalR from '@aspnet/signalr';
-import { env } from 'process';
 import { ChatMessage } from '../models/chatmessage';
 import { environment } from '../../environments/environment';
-import { Message } from '@angular/compiler/src/i18n/i18n_ast';
-
 import { UsersService } from '../services/users.service';
 import { ProfilesService } from '../services/profiles.service';
 import { ClanService } from '../services/clan.service';
@@ -20,12 +17,17 @@ import { FormControl } from '@angular/forms';
 })
 export class ChatComponent implements OnInit, OnDestroy, AfterViewChecked  {
 
+  //the connection our realtime chat uses
   connection: SignalR.HubConnection;
+  //the url the connection connects to
   connectionUrl = environment.baseUrl + "chatHub";
 
+  //the chat input box
   public chatGroup: FormGroup;
 
+  //list of messages
   public Messages: Array<ChatMessage>;
+  //list of online users, not currently implemented
   public OnlineUsers: Array<string>;
 
   constructor(public usersService: UsersService, private profilesService: ProfilesService, public clansService: ClanService, private formBuilder: FormBuilder) {
@@ -35,35 +37,43 @@ export class ChatComponent implements OnInit, OnDestroy, AfterViewChecked  {
   }
 
   ngOnInit(): void {
+    //Builds the connection
     this.connection = new SignalR.HubConnectionBuilder()
       .withUrl(this.connectionUrl)
       .build();
 
+    //Initializes the variables
     this.Messages = new Array<ChatMessage>();
     this.OnlineUsers = new Array<string>();
 
+    //Sets up callbacks for when messages are received
     this.connection.on('ReceiveMessage', this.receiveMessage.bind(this));
     this.connection.on('Init', () => {
       var user = this.usersService.CurrentUser;
       this.connection.invoke("JoinChat", this.clansService.currentClan.clanId, user.userName);
     });
 
+    //starts the connection
     this.connection.start();
   }
 
+  //Stops teh connection when this component is destroyed
   ngOnDestroy(): void {
     this.connection.stop();
   }
 
+  //Scrolls all teh way down after receiving a new messages
   ngAfterViewChecked() {         
     var container = document.getElementById("chatLog");           
     container.scrollTop = container.scrollHeight;
   } 
 
+  //Adds a user to the list of connected users
   public addConnection(username) {
     this.OnlineUsers.push(username);
   }
 
+  //removes a user from the list of connected users
   public removeConnection(username) {
     var index = this.OnlineUsers.findIndex(x => x == username);
     if (index != -1) {
@@ -71,16 +81,19 @@ export class ChatComponent implements OnInit, OnDestroy, AfterViewChecked  {
     }
   }
 
+  //Realtime callback. adds a message to the list
   public receiveMessage(message, sender) {
     var msg = new ChatMessage(sender, message);
     this.Messages = [...this.Messages, msg];
   }
 
+  //Sends a message
   public sendMessage() {
     var message = this.chatGroup.get('message').value;
     if (message == null || message.length == 0) {
       return;
     }
+    //invokes this method on the backend hub and passes in the messages as the parameter
     this.connection.invoke('SendMessage', message);
     this.chatGroup.reset();
   }
