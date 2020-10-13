fc29cbc1064fde05b1fc94ca0aba4b8bc0555044
Steven Lee <stevenlin1090@gmail.com>
2020-10-13 16:31:44 +1100

    unit test added secret q n a

---
+++ Frontend/game-buddy-finder/src/app/forgot-password/forgot-password.component.spec.ts
@@ -0,0 +1,65 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ForgotPasswordComponent } from './forgot-password.component';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterTestingModule } from '@angular/router/testing';
+import { Profile } from '../models/profile';
+
+
+
+describe('ForgotPasswordComponent', () => {
+  let component: ForgotPasswordComponent;
+  let fixture: ComponentFixture<ForgotPasswordComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientModule, RouterTestingModule.withRoutes([])],
+      declarations: [ ForgotPasswordComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ForgotPasswordComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('Secret question should be valid', () => {
+    let SecretQuestion = ("What is your pet name?");
+    expect(component.reset()).toBeTrue(); 
+  });
+
+  it('Secret question should not be valid', () => {
+    let SecretQuestion = ("0011000");
+    expect(component.reset()).toBeFalse(); 
+  });
+
+  it('Secret answer should be valid', () => {
+    let SecretAnswer = ("Sky");
+    expect(component.reset()).toBeTrue(); 
+  });
+
+  it('Secret question should not be valid', () => {
+    let SecretQuestion = ("696969");
+    expect(component.reset()).toBeFalse(); 
+  });
+
+
+
+  
+
+
+
+
+
+
+
+
+
+
+
+});
