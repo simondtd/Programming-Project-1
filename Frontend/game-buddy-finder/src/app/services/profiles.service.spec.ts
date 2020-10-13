0e96788a7d27075846a18f8688a7caa686313aac
Steven Lee <stevenlin1090@gmail.com>
2020-10-13 17:29:37 +1100

    profile validation secret qna

--- Frontend/game-buddy-finder/src/app/services/profiles.service.spec.ts
+++ Frontend/game-buddy-finder/src/app/services/profiles.service.spec.ts
@@ -16,53 +16,53 @@ describe('ProfilesService', () => {
   });
 
   it('Profile should be valid', () => {
-    let profile = new Profile(0, "Simon", "Hesjevik", "SimonH", "aa", "aa", "simon@gmail.com", "Oceania", "", "1");
+    let profile = new Profile(0, "Simon", "Hesjevik", "SimonH", "aa", "aa", "simon@gmail.com", "Oceania", "", "1", "What's your first visited country", "Ireland");
     expect(service.validateUser(profile)).toBeTrue();
   });
 
   it('Profile should be valid', () => {
-    let profile = new Profile(1, "Adama", "Fofana", "AdamaF", "123", "123", "adama@gmail.com", "Oceania", "", "1");
+    let profile = new Profile(1, "Adama", "Fofana", "AdamaF", "123", "123", "adama@gmail.com", "Oceania", "", "1", "What's your first visited country", "China");
     expect(service.validateUser(profile)).toBeTrue();
   });
 
   it('Profile should be valid', () => {
-    let profile = new Profile(2, "Steven", "Lee", "StevenL", "123", "123", "steven@gmail.com", "Indonesia", "", "1");
+    let profile = new Profile(2, "Steven", "Lee", "StevenL", "123", "123", "steven@gmail.com", "Indonesia", "", "1", "What's your first visited country", "Singapore");
     expect(service.validateUser(profile)).toBeTrue();
   });
   it('Profile should not be valid, passwords not equal', () => {
-    let profile = new Profile(2, "Steven", "Lee", "StevenL", "45", "12", "steven@gmail.com", "Indonesia", "", "1");
+    let profile = new Profile(2, "Steven", "Lee", "StevenL", "45", "12", "steven@gmail.com", "Indonesia", "", "1", "What's your first visited country", "Singapore");
     expect(service.validateUser(profile)).toBeFalse();
   });
   it('Profile should not be valid, username is 0', () => {
-    let profile = new Profile(2, "Steven", "Lee", null, "123", "123", "steven@gmail.com", "Indonesia", "", "1");
+    let profile = new Profile(2, "Steven", "Lee", null, "123", "123", "steven@gmail.com", "Indonesia", "", "1", "What's your first visited country", "Singapore");
     expect(service.validateUser(profile)).toBeFalse();
   });
   it('Profile should not be valid, firstnameis 0', () => {
-    let profile = new Profile(1, null, "Fofana", "AdamaF", "123", "123", "adama@gmail.com", "Oceania", "", "1");
+    let profile = new Profile(1, null, "Fofana", "AdamaF", "123", "123", "adama@gmail.com", "Oceania", "", "1", "What's your first visited country", "China");
     expect(service.validateUser(profile)).toBeFalse();
   });
   it('Profile should not be valid, lastname is 0', () => {
-    let profile = new Profile(1, "Adama", null, "AdamaF", "123", "123", "adama@gmail.com", "Oceania", "", "1");
+    let profile = new Profile(1, "Adama", null, "AdamaF", "123", "123", "adama@gmail.com", "Oceania", "", "1", "What's your first visited country", "China");
     expect(service.validateUser(profile)).toBeFalse();
   });
   it('Profile should not be valid, password is 0', () => {
-    let profile = new Profile(1, "Adama", "Fofana", "AdamaF", null, "123", "adama@gmail.com", "Oceania", "", "1");
+    let profile = new Profile(1, "Adama", "Fofana", "AdamaF", null, "123", "adama@gmail.com", "Oceania", "", "1", "What's your first visited country", "China");
     expect(service.validateUser(profile)).toBeFalse();
   });
   it('Profile should not be valid, re-password is 0', () => {
-    let profile = new Profile(1, "Adama", "Fofaa", "AdamaF", "123", null, "adama@gmail.com", "Oceania", "", "1");
+    let profile = new Profile(1, "Adama", "Fofaa", "AdamaF", "123", null, "adama@gmail.com", "Oceania", "", "1", "What's your first visited country", "China");
     expect(service.validateUser(profile)).toBeFalse();
   });
   it('Profile should not be valid, email  is 0', () => {
-    let profile = new Profile(1, "Adama", "Fofana", "AdamaF", "123", "123", null, "Oceania", "", "1");
+    let profile = new Profile(1, "Adama", "Fofana", "AdamaF", "123", "123", null, "Oceania", "", "1", "What's your first visited country", "China");
     expect(service.validateUser(profile)).toBeFalse();
   });
   it('Profile should not be valid, Region is 0', () => {
-    let profile = new Profile(1, "Adama", "Fofana", "AdamaF", "123", "123", "adama@gmail.com", null, "", "1");
+    let profile = new Profile(1, "Adama", "Fofana", "AdamaF", "123", "123", "adama@gmail.com", null, "", "1", "What's your first visited country", "China");
     expect(service.validateUser(profile)).toBeFalse();
   });
   it('Profile should be valid, even though profile pic URL is null', () => {
-    let profile = new Profile(1, "Adama", "Fofana", "AdamaF", "123", "123", "adama@gmail.com", "Oceania", null, "1");
+    let profile = new Profile(1, "Adama", "Fofana", "AdamaF", "123", "123", "adama@gmail.com", "Oceania", null, "1", "What's your first visited country", "China");
     expect(service.validateUser(profile)).toBeTrue();
   });
   it('Profile should be valid, event though mobile Number is 0', () => {
